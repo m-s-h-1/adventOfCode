241af06247431ed81ac356e10e06ea310a442a01
Muhammad Saqib Hussain <saqib9217@live.com>
2022-12-02 12:02:19 +0100

    Day 1 First point done

--- 2022/Day1/src/main.rs
+++ 2022/Day1/src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
 fn main() {
-    println!("Hello, world!");
-}
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let result = input
+        .split("\n\n")
+        .map(|elf_load| {
+            elf_load
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+    result.to_string();
+    println!("{}", result);
+    }
